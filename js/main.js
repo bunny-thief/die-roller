0a878d52727e80c479707dbd32b75a305b4ca98a
bunny-thief <4421229+bunny-thief@users.noreply.github.com>
2023-01-21 14:52:14 +0100

    Add die, random number function, display number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+document.getElementById('die').addEventListener('click', rollDie)
+const h1 = document.getElementById('die')
+
+function rollDie() {
+    let rollValue = Math.floor(Math.random() * 6 + 1)
+    // display random number to console for testing
+    console.log(rollValue)
+    h1.innerText = rollValue
+    return rollValue
+}
+
+// Test random number is between 1 and 6
+// for (let i = 0; i < 100; i++) {
+//     let result = rollDie()
+//     if (result <= 0 || result >= 7) {
+//         console.log('roll failed!')
+//         break
+//     }
+// }
